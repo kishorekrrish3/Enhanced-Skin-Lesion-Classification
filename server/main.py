5a81f39d4d0dcb617706b493dcba60b85ef87ec1
Kishore P <kidkrrish3@gmail.com>
2026-03-31 17:29:13 +0530

    Modified server main.py

--- server/main.py
+++ server/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
 
-from .api.router import router
+from api.router import router
 
 app = FastAPI(title="Skin Lesion API")
 
