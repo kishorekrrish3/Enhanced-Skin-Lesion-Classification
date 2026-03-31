a80e8484cab26845a3a4d58edc4380fc77cbfe59
Kishore P <kidkrrish3@gmail.com>
2026-03-31 19:17:50 +0530

    server main.py updated

--- server/main.py
+++ server/main.py
@@ -1,7 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import os
 
 from server.api.router import router
 
